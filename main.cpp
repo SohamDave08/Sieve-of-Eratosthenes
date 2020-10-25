e1fdb2d9d0fe6e2caca7c366c2c4d8d5ab871d96
Soham Dave <dave.soham2000@gmail.com>
2020-10-25 19:35:07 +0530

    Final method

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+using namespace std;
+void sieve(int n)
+{
+    //? create boolean array
+    bool prime[n+1];
+    memset(prime, true, sizeof(prime));
+
+    for(int p=2;(p*p)<=n;p++)
+    {
+
+        //? If prime[p] ---> False ---> not a prime number
+
+        if(prime[p]==true)
+        {
+            for(int i=(p*p);i<=n;i=i+p)
+                prime[i]=false;
+        }
+    }
+
+    // ? Print prime numbers 
+    for(int i=2;i<=n;i++)
+    {
+        if(prime[i] == true)
+            cout<<i<<" ";
+    }
+}   
+int main()
+{
+    int n;
+    cout<<"Enter the delimiter value: "; 
+    cin>>n;
+    sieve(n);
+    
+    return 0;
+}
